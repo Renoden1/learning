bdb312dad96410b51f95aabe11152134922e79a2
Renoden1 <107457862+Renoden1@users.noreply.github.com>
2022-10-03 18:27:10 +0300

    Delete stock directory

--- stock/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class BuySellForm(forms.Form):
-    price = forms.DecimalField(widget=forms.NumberInput(attrs={'readonly': 'readonly'}))
-    amount = forms.IntegerField()
